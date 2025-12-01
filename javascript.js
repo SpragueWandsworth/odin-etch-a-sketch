84ddbf5a8396feaba867b573b0247bf9e4b358bb
SpragueWandsworth <thetelescope46@gmail.com>
2025-12-01 16:01:41 +0000

    added creating new grid and removing old grid

--- javascript.js
+++ javascript.js
@@ -1,9 +1,18 @@
 //remove outlines at the end
+//customise button size and stuff
 
 const mainContainer = document.querySelector('.main-container');
 const promptButton = document.querySelector('.prompt-button');
 
+let gridSize
+
+promptButton.addEventListener('click', () => createGrid(gridSize = prompt('How many squares do you want per side?')));
+
 function createGrid(rows) {
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+
     if (rows > 100) return;
     for (let i = 1; i <= rows; i++) {
         let column = document.createElement('div');
@@ -18,5 +27,4 @@ function createGrid(rows) {
     };
 };
 
-//let pro = prompt('How many rows do you want?');
 createGrid(16);
